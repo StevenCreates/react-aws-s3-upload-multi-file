966209380394abcc7a90b29006dba75583709eac
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-01 15:13:25 -0600

    multi file upload

---
+++ src/components/Upload.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import S3 from "react-aws-s3";
+
+function Upload() {
+  const fileInput = useRef();
+
+  const config = {
+    bucketName: process.env.REACT_APP_BUCKET_NAME,
+    dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+    region: process.env.REACT_APP_REGION,
+    accessKeyId: process.env.REACT_APP_ACCESS_ID,
+    secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    let newArr = fileInput.current.files;
+    for (let i = 0; i < newArr.length; i++) {
+      handleUpload(newArr[i]);
+    }
+  };
+
+  const handleUpload = (file) => {
+    let newFileName = file.name.replace(/\..+$/, "");
+    const ReactS3Client = new S3(config);
+    ReactS3Client.uploadFile(file, newFileName).then((data) => {
+      console.log(data);
+      if (data.status === 204) {
+        console.log("success");
+      } else {
+        console.log("fail");
+      }
+    });
+  };
+
+  return (
+    <>
+      <form className='upload-steps' onSubmit={handleClick}>
+        <label>
+          Upload file:
+          <input type='file' multiple ref={fileInput} />
+        </label>
+        <br />
+        <button type='submit'>Upload</button>
+      </form>
+    </>
+  );
+}
+export default Upload;
